c5d903bb7a6c84ef891d09f1e8bdca3af6b7d12f
hishamrehab <hishamrehab309@gmail.com>
2024-01-28 03:56:27 +0200

    video detail

--- src/components/ChannelDetail.jsx
+++ src/components/ChannelDetail.jsx
@@ -1,7 +1,33 @@
-import React from 'react'
+import React from "react";
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { Box } from "@mui/material";
+import { Videos, ChannelCard } from "./index";
+import { fetchFromApi } from "../utils/fetchFromApi";
 
 const ChannelDetail = () => {
-  return <div>ChannelDetail</div>;
+  const [channelDetail, setChannelDetail] = useState(null);
+  const [videos, setVideos] = useState([]);
+  const { id } = useParams();
+
+  
+  useEffect(() => {
+    fetchFromApi(`channels?part=snippet&id=${id}`).then((data) =>
+      setChannelDetail(data?.items[0])
+    );
+     console.log(videos);
+     
+
+    fetchFromApi(`search?channelId=${id}&part=snippet&id=&
+        order=date
+       `).then((data) => setVideos(data?.items));
+  }, [id]);
+
+  return <div>{id}</div>;
 };
 
 export default ChannelDetail;
+
+
+
+  
--- src/components/VideoCard.js
+++ src/components/VideoCard.js
@@ -1,27 +1,75 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { Typography, Card, CardMedia } from "@mui/material";
+import { Typography, Card, CardContent, CardMedia } from "@mui/material";
 import { CheckCircle } from "@mui/icons-material";
 
-export const demoThumbnailUrl = "https://i.ibb.co/G2L2Gwp/API-Course.png";
-export const demoVideoUrl = "/video/GDa8kZLNhJ4";
-export const demoVideoTitle =  "Build and Deploy 5 JavaScript & React API Projects in 10 Hours - Full Course | RapidAPI";
-export const demoChannelUrl = "/channel/UCmXmlB4-HJytD7wek0Uo97A";
-export const demoChannelTitle = "JavaScript Mastery";
-
-
+import {
+  demoThumbnailUrl,
+  demoChannelUrl,
+  demoVideoUrl,
+  demoChannelTitle,
+  demoVideoTitle,
+} from "../utils/constants";
 
 const VideoCard = ({
   video: {
     id: { videoId },
     snippet,
   },
-  
 }) => {
-  return <Card>
-  <CardMedia image={snippet?.thumbnails?Card.high?.url }/>
+  return (
+    <Card
+      sx={{
+        width: { md: "320px", sx: "100%" },
+        boxShadow: "none",
+        borderRadius: "0 ",
+      }}
+    >
+      <Link to={videoId ? `/video/${videoId}` : demoVideoUrl}>
+        <CardMedia
+          sx={{
+            width: 358,
+            height: 180,
+            objectFit: "fill",
+          }}
+          image={snippet?.thumbnails?.high?.url}
+          alt={snippet?.title}
+        />
+      </Link>
 
-  </Card>;
+      <CardContent
+        sx={{
+          backgroundColor: "#1e1e1e",
+          height: "106px",
+        }}
+      >
+        <Link to={videoId ? `/video/${videoId}` : demoVideoUrl}>
+          <Typography variant="subtitle1" fontWeight="bold" color="#FFF">
+            {snippet?.title.slice(0, 60) || demoVideoTitle.slice(0, 60)}
+          </Typography>
+        </Link>
+
+        <Link
+          to={
+            snippet?.channelId
+              ? `/channel/${snippet?.channelId}`
+              : demoChannelUrl
+          }
+        >
+          <Typography variant="subtitle1" fontWeight="bold" color="#FFF">
+            {snippet?.channelTitle || demoChannelTitle}
+            <CheckCircle
+              sx={{
+                fontSize: 12,
+                color: "gray",
+                ml: "5px",
+              }}
+            />
+          </Typography>
+        </Link>
+      </CardContent>
+    </Card>
+  );
 };
 
 export default VideoCard;
